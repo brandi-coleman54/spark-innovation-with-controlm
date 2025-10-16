47792c02c3bc472333baf623229c841817ae0e58
jfink5 <95642468+jfink5@users.noreply.github.com>
2025-10-16 12:46:29 -0400

    Create analyze_whiskey_data.py

---
+++ whiskey-retail/dags/analyze_whiskey_data.py
@@ -0,0 +1,17 @@
+from airflow.operators.bash import BashOperator
+from airflow.models.dag import DAG
+from datetime import datetime
+
+with DAG(
+    dag_id='analyze_whiskey_data',
+    start_date=datetime(2023, 1, 1),
+    schedule=None,
+    catchup=False,
+    params={
+        "user_code": "tus"
+    }
+) as dag:
+    run_background_script = BashOperator(
+        task_id='run_background_script',
+        bash_command='cd /root/airflow/scripts && nohup /root/airflow/scripts/analyze_whiskey_data.sh ${CTM_USER_CODE} &'
+    )
