188cea1d5e563f4b9eb950deee5d49402964d550
jfink5 <95642468+jfink5@users.noreply.github.com>
2025-10-16 12:35:35 -0400

    Create employee.py

---
+++ whiskey-retail/dags/employee.py
@@ -0,0 +1,42 @@
+# To initiate the DAG Object
+from airflow import DAG
+# Importing datetime and timedelta modules for scheduling the DAGs
+from datetime import timedelta, datetime
+# Importing operators 
+from airflow.operators.empty import EmptyOperator
+from airflow.providers.amazon.aws.operators.lambda_function import LambdaInvokeFunctionOperator
+import os
+from airflow.models.connection import Connection
+
+# Initiating the default_args
+default_args = {
+        'owner' : 'airflow',
+        'start_date' : datetime(2024, 8, 5)
+}
+
+
+with DAG(
+    dag_id='employee_functions',
+    schedule=None,
+    start_date=datetime(2021, 1, 1),
+    dagrun_timeout=timedelta(minutes=60),
+    tags=['employee_data'],
+    is_paused_upon_creation=False,
+    catchup=False,
+) as dag:
+    # [START howto_lambda_operator]
+    employeeLambdaFunc0 = LambdaInvokeFunctionOperator(
+        task_id='set_employee_data_func',
+        aws_conn_id='aws_default',
+        function_name='ctmSetEmployeeData',
+        payload='',
+    ),
+    employeeLambdaFunc1 = LambdaInvokeFunctionOperator(
+        task_id='norm_employee_data_func',
+        aws_conn_id='aws_default',
+        function_name='ctmNormEmployeeData',
+        payload='',
+    )
+
+    employeeLambdaFunc0 >> employeeLambdaFunc1
+    # [END howto_lambda_operator]
