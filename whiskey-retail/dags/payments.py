db84c07dac1d4658e2b22a692dad697b8cc28688
jfink5 <95642468+jfink5@users.noreply.github.com>
2025-10-16 12:38:17 -0400

    Create payments.py

---
+++ whiskey-retail/dags/payments.py
@@ -0,0 +1,36 @@
+# To initiate the DAG Object
+from airflow import DAG
+# Importing datetime and timedelta modules for scheduling the DAGs
+from datetime import timedelta, datetime
+# Importing operators 
+from airflow.operators.empty import EmptyOperator
+from airflow.providers.amazon.aws.operators.lambda_function import LambdaInvokeFunctionOperator
+import os
+from airflow.models.connection import Connection
+
+# Initiating the default_args
+default_args = {
+        'owner' : 'airflow',
+        'start_date' : datetime(2024, 8, 5)
+}
+
+
+with DAG(
+    dag_id='payments_function',
+    schedule=None,
+    start_date=datetime(2021, 1, 1),
+    dagrun_timeout=timedelta(minutes=60),
+    tags=['payments_data'],
+    is_paused_upon_creation=False,
+    catchup=False,
+) as dag:
+    # [START howto_lambda_operator]
+    paymentsLambdaFunc0 = LambdaInvokeFunctionOperator(
+        task_id='set_payments_data_func',
+        function_name='ctmGetPaymentsData',
+        aws_conn_id='aws_default',
+        payload='',
+    )
+
+    paymentsLambdaFunc0
+    # [END howto_lambda_operator]
