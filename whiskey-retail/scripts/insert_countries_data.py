7c75633c529ab9c434aa581cbd07f7f57b5887a4
jfink5 <95642468+jfink5@users.noreply.github.com>
2025-10-17 17:01:15 -0400

    Update insert_countries_data.py

--- whiskey-retail/scripts/insert_countries_data.py
+++ whiskey-retail/scripts/insert_countries_data.py
@@ -8,20 +8,11 @@ pg_host = os.environ['POSTGRES_POSTGRESQL_SERVICE_HOST']
 pg_password = os.environ['PG_PASSWORD']
 db_prefix = sys.argv[1]
 
-connection = psycopg2.connect(host=pg_host, port=int(5432), user='postgres',password=pg_password)
+connection = psycopg2.connect(host=pg_host, port=int(5432), user='postgres',password=pg_password, database=f"{db_prefix}_whiskey_retail_shop")
 cursor = connection.cursor()
-cursor.execute(f"SET search_path = {db_prefix}_whiskey_retail_shop;")
-
-# Connecting Python to MySQL
-import pymysql
 
 def sql(query):
     return ps.sqldf(query)
-
-
-#query = "TRUNCATE countries"
-#cursor.execute(query)
-#connection.commit()
 
 columns_to_read = ['Country', 'Country_Code', 'country_id']
 df = pd.read_csv("/mft_mountPath/countries.csv", usecols=columns_to_read)
@@ -33,4 +24,5 @@ cursor.executemany(query, records)
 
 
 # Commit the transaction
+
 connection.commit()
