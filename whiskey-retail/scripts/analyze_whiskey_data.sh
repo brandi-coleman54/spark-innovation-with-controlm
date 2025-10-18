cde4f5920e0f16a58c918f9ae0fd5ac0e3afe357
jfink5 <95642468+jfink5@users.noreply.github.com>
2025-10-18 13:18:32 -0400

    Update analyze_whiskey_data.sh

--- whiskey-retail/scripts/analyze_whiskey_data.sh
+++ whiskey-retail/scripts/analyze_whiskey_data.sh
@@ -3,4 +3,4 @@
 user_code=$1
 
 source /root/scripts/venv/bin/activate
-streamlit run analyze_data.py -- ${user_code} --server.headless true --server.port 8501
+nohup streamlit run analyze_data.py -- ${user_code} --server.headless true --server.port 8501 &
