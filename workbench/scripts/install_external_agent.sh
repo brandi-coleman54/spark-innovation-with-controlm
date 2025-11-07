c7c3d1f0e987a094fb467e68c7a6e0113aaad23f
jfink5 <95642468+jfink5@users.noreply.github.com>
2025-11-07 09:59:50 -0500

    Create install_external_agent.sh

---
+++ workbench/scripts/install_external_agent.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+USER=controlm
+USER_HOME=/home/controlm
+AGENT_NAME=workbench-host
+AGENT_PORT=7006
+SERVER_PORT=7005
+SERVER_HOST=workbench
+SERVER_NAME=workbench
+CONTAINER_NAME=workbench
+
+EXIT_CODE=1
+TRIES=0
+MAX_TRIES=10
+
+# Wait for workbench to complete startup
+while [[ "${EXIT_CODE}" -ne "0" ]]; do
+  docker logs ${CONTAINER_NAME} | grep "Completed"
+  EXIT_CODE=$?
+  if [[ "${EXIT_CODE}" -ne "0" ]]; then
+    ((TRIES++))
+    if [[ "${TRIES}" -le "${MAX_TRIES}" ]]; then
+      sleep 30
+    else
+      echo "Error - exceeded max tries checking container ${CONTAINER_NAME}!"
+      break
+    fi
+  fi
+done
+
+function Create_Silent_File {
+
+    agent_port=$1
+    server_port=$2
+    server_host=$3
+    server_name=$4
+
+    cat > ${USER_HOME}/silent_install_agent.xml <<EOF
+<AutomatedInstallation langpack="eng">
+    <target.product>Control-M  Agent</target.product>
+    <agent.parameters>
+        <entry value="7035" key="ctm_agent.Tracker.Event.Port"/>
+        <entry value="" key="ctm_agent.Force.Upgrade"/>
+        <entry value="" key="Ignore.Disabling.Agent.Failure"/>
+        <entry value="${agent_port}" key="field.Server.To.Agent.Port.Number"/>
+        <entry value="${server_port}" key="field.Agent.To.Server.Port.Number"/>
+        <entry value="${server_host}" key="field.Primary.Controlm.Server.Host"/>
+        <entry value="${server_name}" key="field.Authorized.Controlm.Server.Host"/>
+        <entry value="60" key="ctm_agent.Tcp_ip.Timeout"/>
+    </agent.parameters>
+</AutomatedInstallation>
+EOF
+
+}
+
+function Run_Silent_Install {
+
+    agent_name=$1
+
+    cd ${USER_HOME}
+
+    su - ${USER} bash -c "${USER_HOME}/setup.sh -silent ${USER_HOME}/silent_install_agent.xml"
+    su - ${USER} bash -c '''source ~/.bashrc &&
+        ctmcfg -table CONFIG -action update -parameter LOGICAL_AGENT_NAME -value ${AGENT_NAME} &
+        ctmcfg -table CONFIG -action update -parameter PERSISTENT_CONNECTION -value Y &
+        shut-ag -u controlm -p ALL &&
+        start-ag -u controlm -p ALL'''
+
+}
+
+
+Create_Silent_File ${AGENT_PORT} ${SERVER_PORT} ${SERVER_HOST} ${SERVER_NAME}
+Run_Silent_Install ${AGENT_NAME}
