020526a4b8bc0b0ccb74fa33c18a6321d7d61cbe
jfink5 <95642468+jfink5@users.noreply.github.com>
2025-11-24 13:57:55 -0500

    Create agent_test.sh

---
+++ common/scripts/agent_test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+CTM_SERVER=$1
+CTM_AGENT=$2
+
+function Test_Agent {
+
+    ctm_server=$1
+    ctm_agent=$2
+
+    count=1
+    limit=2
+    while [ "${count}" -le "${limit}" ]; do
+
+        echo "Agent test ${count}"
+        ctm config server:agent::test ${ctm_server} ${ctm_agent}
+        if [ "$?" -eq "0" ]; then
+            break
+        else
+            ((count++))
+            if [[ "${count}" -gt "${limit}" ]]; then
+                echo "Timed out waiting for agent to become available"
+            else
+                sleep 10
+            fi
+        fi
+        
+    done
+
+}
+
+Test_Agent ${CTM_SERVER} ${CTM_AGENT}
