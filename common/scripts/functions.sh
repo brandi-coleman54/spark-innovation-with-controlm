b579b7c4267df8f244a08035a07f768b34fba933
jfink5 <95642468+jfink5@users.noreply.github.com>
2026-01-28 09:33:18 -0500

    Update functions.sh

--- common/scripts/functions.sh
+++ common/scripts/functions.sh
@@ -297,7 +297,7 @@ function Provision_Agents_Helm {
     --set api.endpoint="${ctm_aapi_endpoint}" \
     --set api.token="${ctm_auth_token}" \
     --set agent.tag="sparkit" \
-    --set agent.replicas="2" \
+    --set agent.replicas="1" \
     --set pvc.storageClass="local-path" \
     --set pvc.volumeSize="1Gi" \
     --set pvc.accessMode="ReadWriteOnce" \
