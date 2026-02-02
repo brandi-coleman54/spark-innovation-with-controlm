17cf3afc7a59a4f7a9a886a983006c5319c956f7
jfink5 <95642468+jfink5@users.noreply.github.com>
2026-02-02 13:05:14 -0500

    Update setup.sh

--- sp500-analytics/scripts/setup.sh
+++ sp500-analytics/scripts/setup.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# (optional) create a venv
-cd /home/controlm/labs
-python -m venv .venv
-source .venv/bin/activate
+source ~/venv/bin/activate
 
 # install deps
 pip install streamlit pandas altair pyarrow numpy
