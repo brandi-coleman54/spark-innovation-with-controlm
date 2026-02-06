61bf0e51596251f0caf0db642f2b93da5aab9a0e
jfink5 <95642468+jfink5@users.noreply.github.com>
2026-02-06 16:25:32 -0500

    Update install_saas_agent_mft.sh

--- common/scripts/install_saas_agent_mft.sh
+++ common/scripts/install_saas_agent_mft.sh
@@ -44,7 +44,7 @@ done
 
 tries=0
 until ctm config server:agent:mft:ssh:key::generate IN01 ${target} ${user_code}_id ${user_code}${user_code} 2048 -e ${ctm_env}; do
-    tries=$((tries +1))
+    tries=$((tries + 1))
   if [ "$tries" -ge 10 ]; then
     echo "Failed config ssh after $tries attempt."
     ctm env del ${ctm_env}
