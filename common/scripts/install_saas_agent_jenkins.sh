fe2f9099d1bdeb2565c5c726a88bfc4b1a5ef2d2
brandi-coleman54 <brandi.coleman54@gmail.com>
2026-02-06 09:43:35 -0500

    Create install_saas_agent_jenkins.sh

---
+++ common/scripts/install_saas_agent_jenkins.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+user_code=$1
+api_endpoint=$2
+api_token=$3
+ctm_env=agent_provisioning
+
+# Run from /home/controlm
+cd /home/controlm || {
+    echo "Error: cannot cd to /home/controlm"
+    exit 3
+}
+
+tries=0
+target="${user_code}_instruqt_server"
+
+ctm env add ${ctm_env} ${api_endpoint} ${api_token}
+
+until ctm provision saas::install Agent_Ubuntu.Linux sparkit ${target} -e ${ctm_env}; do
+    ((tries++))
+    if [ "${tries}" -ge "3" ]; then
+        echo "Install failed after ${tries} attempts."
+        ctm env del admin
+        exit 4
+    fi
+    echo "Install failed (attempt ${tries}). Retrying in 10s..."
+    sleep 10
+done
+
+
+# --- Install Jenkins plugin ---
+tries=0
+until ctm provision image Jenkins_plugin.Linux; do
+  ((tries++))
+  if [ "\$tries" -ge 3 ]; then
+    echo "Failed installation of Jenkins plugin after \$tries attempt."
+    ctm env del admin
+    exit 4
+  fi
+  echo "Installation failed (attempt \$tries). Retrying in 10s..."
+  sleep 10
+done
+
+ctm deploy /home/controlm/labs/forecast-flow-jenkins/local_repo/cp_jenkins.json
+
+ctm env del ${ctm_env}
+echo "Agent install invoked successfully for ${target}."
