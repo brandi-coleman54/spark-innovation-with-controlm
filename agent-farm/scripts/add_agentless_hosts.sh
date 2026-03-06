4e8a7961fcabec2979d36129ca77c24093b91bcc
jfink5 <95642468+jfink5@users.noreply.github.com>
2026-03-06 16:12:20 -0500

    Update add_agentless_hosts.sh

--- agent-farm/scripts/add_agentless_hosts.sh
+++ agent-farm/scripts/add_agentless_hosts.sh
@@ -5,6 +5,7 @@ SERVER=$2
 OPER=$3
 NAME=$4
 COUNT=${5:-}
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # Build assoc agents array string
 assoc_ct=$(ctm config server:agents::get ${SERVER} -s "agent=${PREFIX}*" | jq '.agents | length'
@@ -14,7 +15,15 @@ for ((i=0; i<assoc_ct; i++)); do
 done
 assoc_str="${assoc_str%,}"
 
+assoc_agents=${assoc_str} envsubst < ${SCRIPT_DIR}/../templates/agentless_config.json > agentless_config.json
 
 
-if [[ "$OPER" == "single" ]]; then
-  ctm config server:agentlesshost::add <server> <agentlesshost>
+if [[ "${OPER}" == "single" ]]; then
+  ctm config server:agentlesshost::add ${SERVER} ${NAME} -f agentless_config.json
+fi
+
+if [[ "${OPER}" == "count" ]]; then
+  for ((i=1; i<=${COUNT}; i++)); do
+    ctm config server:agentlesshost::add ${SERVER} ${NAME}${i} -f agentless_config.json
+  done
+fi
