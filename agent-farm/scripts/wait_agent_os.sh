9d01973413ea362ec7d7e92a2b79e186db09b0c8
jfink5 <95642468+jfink5@users.noreply.github.com>
2026-03-09 10:36:30 -0400

    Update wait_agent_os.sh

--- agent-farm/scripts/wait_agent_os.sh
+++ agent-farm/scripts/wait_agent_os.sh
@@ -9,9 +9,9 @@ for ((i=1; i<=${COUNT}; i++)); do
   agent=${PREFIX}${i}
   os=$(ctm config server:agents::get ${SERVER} -s "agent=${agent}" | jq -r '.agents[0].operatingSystem')
   tries=1
-  until [[ "${os} != "" ]]; do
+  until [[ "${os}" != "" ]]; do
     echo "Try for OS ${tries}"
-    if [ "${tries}" -ge 20 ]; then
+    if [ "${tries}" -ge "20" ]; then
       echo "breaking"
       break
     fi
