953c7343bbdfdccc86d0552b9fe95b4ee1855437
jfink5 <95642468+jfink5@users.noreply.github.com>
2026-03-17 17:15:23 -0400

    Update add_agentless_policies.sh

--- agent-farm/scripts/add_agentless_policies.sh
+++ agent-farm/scripts/add_agentless_policies.sh
@@ -7,5 +7,11 @@ RUNNIN_JOBS=$4
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 for ((i=1; i<=AGENT_COUNT; i++)); do
-  policy_name=AGENTLESS${i}_THROTTLE agent_name=${AGENT_PREFIX}${i}
+  policy_name=AGENTLESS${i}_THROTTLE \
+  agent_name=${AGENT_PREFIX}${i} \
+  running_jobs=${RUNNING_JOBS} \
+  server=${SERVER} \
+    envsubst < ${SCRIPT_DIR}/../templates/wp_throttle_agentless.json > /tmp/wp_${i}.json
+  ctm run workloadpolicies::add /tmp/wp_${i}.json
+  ctm run workloadpolicies::activate AGENTLESS${i}_THROTTLE
 done
