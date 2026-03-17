30081d56b6ff2fddcdf0fb72b4e31d08e254cd71
jfink5 <95642468+jfink5@users.noreply.github.com>
2026-03-17 17:16:54 -0400

    Update add_agentless_policies.sh

--- agent-farm/scripts/add_agentless_policies.sh
+++ agent-farm/scripts/add_agentless_policies.sh
@@ -3,7 +3,7 @@
 SERVER=$1            # ctm server name
 AGENT_PREFIX=$2      # this is the prefix for the agentless name
 AGENT_COUNT=$3      # number of agentless machines to add
-RUNNIN_JOBS=$4
+RUNNING_JOBS=$4
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 for ((i=1; i<=AGENT_COUNT; i++)); do
